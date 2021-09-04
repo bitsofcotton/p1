1247d778cf3251c5bc779cd0b48cfd9efcae920d
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-05 00:33:26 +0900

    shrink parameters.

--- p1.cc
+++ p1.cc
@@ -23,33 +23,24 @@ int main(int argc, const char* argv[]) {
 #define int int64_t
 #endif
   std::cout << std::setprecision(30);
-  int  stat(80);
-  int  var(sqrt(num_t(int(stat))));
-  int  step(1);
+  int  stat(64);
   if(argc < 2)
-    std::cerr << "p1 <condition>? <var>? <step>?" << std::endl;
-  else {
-    if(1 < argc) stat = std::atoi(argv[1]);
-    if(2 < argc) var  = std::atoi(argv[2]);
-    else var = max(2, int(sqrt(num_t(abs(int(stat))))));
-    if(3 < argc) step = std::atoi(argv[3]);
-  }
-  std::cerr << "continue with p1 " << stat << " " << var << " " << step << std::endl;
-  P1I<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(abs(stat), var, step);
-  P1I<num_t, arctanFeeder<num_t, idFeeder<num_t> > > q(abs(stat), var, step);
+    std::cerr << "p1 <condition>?" << std::endl;
+  if(1 < argc) stat = std::atoi(argv[1]);
+  const auto var(min(8, max(2, int(sqrt(num_t(abs(int(stat))))))));
+  std::cerr << "continue with p1 " << stat << std::endl;
+  P1I<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(abs(stat), var);
+  P1I<num_t, arctanFeeder<num_t, idFeeder<num_t> > > q(abs(stat), var);
   std::string s;
   num_t d(0);
   auto  D(d);
-  std::vector<num_t> M;
-  M.resize(step, num_t(0));
+  auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
-    D  = d * M[0];
-    for(int i = 1; i < M.size(); i ++)
-      M[i - 1] = M[i];
-    M[M.size() - 1] = stat < 0 ? q.next(d) : p.next(d);
-    std::cout << D << ", " << M[M.size() - 1] << ", " << M[0] << ", " << d << std::endl << std::flush;
+    D  = d * M;
+    M = stat < 0 ? q.next(d) : p.next(d);
+    std::cout << D << ", " << M << ", " << d << std::endl << std::flush;
   }
   return 0;
 }
