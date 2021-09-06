7b6cea3440e428ba1552260fcf328f7ae6e26147
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-07 01:35:37 +0900

    p1 will freeze with this.

--- p1.cc
+++ p1.cc
@@ -23,11 +23,12 @@ int main(int argc, const char* argv[]) {
 #define int int64_t
 #endif
   std::cout << std::setprecision(30);
-  int  stat(64);
+  // N.B. R^8 with index, we use const. twice, R^7 is at last.
+  int  stat(49);
   if(argc < 2)
     std::cerr << "p1 <condition>?" << std::endl;
   if(1 < argc) stat = std::atoi(argv[1]);
-  stat = max(min(stat, int(64)), - int(64));
+  stat = max(min(stat, int(49)), - int(49));
   const auto var(min(8, max(2, int(sqrt(num_t(abs(int(stat))))))));
   std::cerr << "continue with p1 " << stat << std::endl;
   P1I<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(abs(stat), var);
