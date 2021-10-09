8ae60707094d1a7ccce5654cd3367d2e0e38c725
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-10-09 23:40:37 +0900

    ok.

--- p1.cc
+++ p1.cc
@@ -41,9 +41,22 @@ int main(int argc, const char* argv[]) {
     std::cerr << "p1 <step>?" << std::endl;
   if(1 < argc) step = std::atoi(argv[1]);
   std::cerr << "continue with p1 " << step << std::endl;
-  // N.B. we need original and pair-wise to denoise.
-  plin_t  p(plin_pt( stat * abs(step), var, abs(step)), abs(step));
-  patan_t q(patan_pt(stat * abs(step), var, abs(step)), abs(step));
+  // please refer bitsofcotton/p2/catgp.cc
+  vector<plin_t>  p;
+  vector<patan_t> q;
+  if(step < 0) {
+    q.reserve(abs(step));
+    for(int i = 0; i < abs(step); i ++) {
+      const int ss(pow(num_t(int(2)), num_t(int(i))));
+      q.emplace_back(patan_t(patan_pt(stat * ss, var, ss), ss));
+    }
+  } else {
+    p.reserve(abs(step));
+    for(int i = 0; i < abs(step); i ++) {
+      const int ss(pow(num_t(int(2)), num_t(int(i))));
+      p.emplace_back(plin_t(plin_pt(stat * ss, var, ss), ss));
+    }
+  }
   std::string s;
   num_t d(0);
   auto  M(d);
@@ -51,7 +64,10 @@ int main(int argc, const char* argv[]) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    std::cout << D << ", " << (M = step < 0 ? q.next(d) : p.next(d)) << std::endl << std::flush;
+    M = num_t(int(0));
+    for(int i = 0; i < (step < 0 ? q.size() : p.size()); i ++)
+      M += step < 0 ? q[i].next(d) : p[i].next(d);
+    std::cout << D << ", " << M << std::endl << std::flush;
   }
   return 0;
 }
