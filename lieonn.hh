2f46ad9a2437108e2aa6a255eb26b5895575679e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-24 18:01:20 +0900

    shrink matrix change not on sum on delta.

--- lieonn.hh
+++ lieonn.hh
@@ -3311,21 +3311,20 @@ public:
   }
   inline ~shrinkMatrix() { ; }
   inline T next(const T& in) {
-    if(noend) { d[0] += in; ++ t; }
-    else d[int(t ++) % d.size()] = in;
-    const T dsize(noend ? t : myuint(min(int(t), int(d.size()))));
+    if(noend) { d[0] += in; }
+    else d[(t ++) % d.size()] = in;
+    const T dsize(min(t, int(d.size())));
     auto D(d[0]);
     for(int i = 1; i < d.size(); i ++) D += d[i];
-    m[int(t) % m.size()] = noend
-      ?  p.next(D / dsize) * (dsize + T(int(1))) - D
-      : (p.next(D / dsize) *  dsize - D) / dsize + in;
+    // N.B. for each delta.
+    m[t % m.size()] = noend ? p.next(D) - D : p.next(D / dsize);
     auto res(m[0]);
     for(int i = 1; i < m.size(); i ++) res += m[i];
     return res /= T(int(m.size()));
   }
 private:
   P p;
-  myuint t;
+  int t;
   vector<T> d;
   vector<T> m;
 };
