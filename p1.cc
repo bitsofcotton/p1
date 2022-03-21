0d58f9e0209cefa90602fcb4e7a0790a1887c600
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-03-21 21:34:35 +0900

    default value and usage change.

--- p1.cc
+++ p1.cc
@@ -30,11 +30,10 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   const auto stat(3 * 3);
   const auto var(3);
-        int  step(4);
-  if(argc < 2)
-    std::cerr << argv[0] << " <step>?" << std::endl;
+        int  step(1);
+  if(argc < 2) std::cerr << argv[0] << " <step>? : continue with ";
   if(1 < argc) step = std::atoi(argv[1]);
-  std::cerr << "continue with " << argv[0] << " " << step << std::endl;
+  std::cerr << argv[0] << " " << step << std::endl;
   shrinkMatrix<num_t, plin_t> p;
   plin_t q;
   if(step < 0) q = plin_t(max(abs(step) + int(3), (step - 1) * (step - 1)), abs(step));
