d4fb1830169fb1609964767bcc86a98ec49466fb
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-08-27 19:56:59 +0900

    the ratio had a matter, revert another parts.

--- lieonn.hh
+++ lieonn.hh
@@ -2657,8 +2657,7 @@ template <typename T> static inline pair<SimpleVector<T>, T> makeProgramInvarian
 }
 
 template <typename T> static inline T revertProgramInvariant(const pair<T, T>& in) {
-  auto res(abs(in.first * in.second));
-  return (res - floor(res / T(int(2))) * T(int(2))) - T(int(1));
+  return max(T(int(0)), min(T(int(2)), abs(in.first * in.second) )) - T(int(1));
 }
 
 template <typename T> class idFeeder {
